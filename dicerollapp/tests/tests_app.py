cc7c090ac81e9e62d46f88cbd428c4b2bfbb626c
nico <raraujo@thoughtworks.com>
2016-03-29 22:04:08 -0300

    adding test for validation of empty diceroll description

--- dicerollapp/tests/tests_app.py
+++ dicerollapp/tests/tests_app.py
@@ -1,6 +1,8 @@
 from django_webtest import WebTest
 from django.core.urlresolvers import reverse
 
+from dicerollapp.models import DiceRoll
+
 class HomeTest(WebTest):
     def test_display_new_diceroll_form_on_home(self):
         response = self.app.get(reverse('home'))
@@ -14,3 +16,11 @@ class HomeTest(WebTest):
         response = response.follow()
 
         self.assertContains(response, 'test description')
+
+    def test_form_submit_empty_error(self):
+        response = self.app.get(reverse('home'))
+        response.form['description'] = ''
+        response = response.form.submit()
+        self.assertFormError(response, 'form', 'description', u'This field is required.')
+
+def 
